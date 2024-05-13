87590a14759fa5e4fcd5e14db04a54cc8d63eab6
Jeferson <jcastanoossa@unicauca.edu.co>
2024-05-13 00:00:22 -0500

    CORS agregado

--- node-server/src/index.js
+++ node-server/src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const v1HorarioRouter = require("./routes/v1/horarioRoutes");
 const v1UserRouter = require("./routes/v1/userRoutes");
@@ -6,6 +7,7 @@ const v1UserRouter = require("./routes/v1/userRoutes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/api/v1/users", v1UserRouter);
