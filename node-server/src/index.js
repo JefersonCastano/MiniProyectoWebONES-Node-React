97f03a1ced8e2d7fae84d3939388f608666c0c57
Jeferson <jcastanoossa@unicauca.edu.co>
2024-05-15 18:20:29 -0500

    BasePath en las rutas

--- node-server/src/index.js
+++ node-server/src/index.js
@@ -11,19 +11,19 @@ const v1ProgramaRouter = require("./routes/v1/programaRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const v1BasePath = "/api/v1/";
 
 app.use(cors());
 
 app.use(express.json());
 app.use(morgan("dev"));
-app.use("/api/v1/users", v1UserRouter);
-app.use("/api/v1/horarios", v1HorarioRouter);
-app.use("/api/v1/docentes", v1DocenteRouter);
-app.use("/api/v1/periodo_academico", v1PeriodoAcademicoRouter);
-app.use("/api/v1/ambiente", v1AmbienteRouter);
-app.use("/api/v1/competencia", v1competenciaRouter);
-app.use("/api/v1/programa", v1ProgramaRouter);
+app.use(v1BasePath + "users", v1UserRouter);
+app.use(v1BasePath + "horarios", v1HorarioRouter);
+app.use(v1BasePath + "docentes", v1DocenteRouter);
+app.use(v1BasePath + "periodo_academico", v1PeriodoAcademicoRouter);
+app.use(v1BasePath + "ambiente", v1AmbienteRouter);
+app.use(v1BasePath + "competencia", v1competenciaRouter);
+app.use(v1BasePath + "programa", v1ProgramaRouter);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server listening on port ${PORT}`);
