21d9622c73289a9b2ebb7e30bcb983e37b744dc5
Jeferson <jcastanoossa@unicauca.edu.co>
2024-05-18 14:24:02 -0500

    ID programa a String

--- node-server/src/repository/models/Programa.js
+++ node-server/src/repository/models/Programa.js
@@ -3,7 +3,7 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   return sequelize.define('programa', {
     programa_id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.STRING(10),
       primaryKey: true
     },
     programa_nombre: DataTypes.STRING(50)
